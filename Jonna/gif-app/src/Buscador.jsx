777ecc943bc25a432f9a3b4d28aa5ce466e86874
Jonnaespinoza123 <jonnathan.espinoza@cecytesinaloa.edu.mx>
2024-09-19 16:37:11 -0700

    Mi nueva carpeta

---
+++ Jonna/gif-app/src/Buscador.jsx
@@ -0,0 +1,7 @@
+export const Buscador = ({onSubmit, valorInput, onChange }) => {
+    return (
+        <form className="form-buscador" onSubmit={onSubmit}>
+        <input className="buscador" value={valorInput} onChange={onChange}/>
+        </form>
+    )
+}
