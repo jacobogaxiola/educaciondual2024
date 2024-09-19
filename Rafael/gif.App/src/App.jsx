aeafbbc17446d5c2fa9416d285ef6cb37d5693f4
Rafaelparracamacho <rafael.parra@cecytesinaloa.edu.mx>
2024-09-19 16:48:06 -0700

    saracatunga

---
+++ Rafael/gif.App/src/App.jsx
@@ -0,0 +1,40 @@
+import { Buscador } from "./Buscador";
+import { GridGifs } from "./GridGifs";
+import { useSearchGifs } from "./useSearchGifs";
+
+function App() {
+  const {valorInput, onChange, onSubmit, gifs, estaCargando} = useSearchGifs()
+
+  return (
+    <div className="App">
+
+      <Buscador 
+      valorInput={valorInput}
+      onChange={onChange}
+      onSubmit={onSubmit}
+      
+      />
+
+  {
+      estaCargando ? 
+      ( <h2 className="h2">Cargando...</h2>) :
+      (
+      <GridGifs 
+        gifs={gifs}
+        />
+      )
+}
+
+   
+      
+      
+      
+
+    </div>
+
+  
+
+  )
+}
+
+export default App
