24470ed1e0d717cd90bc2edeb022dfa7c4de6b18
edgarvilla123 <edgar.soto@cecytesinaloa.edu.mx>
2024-09-23 16:53:38 -0700

    wenas

---
+++ edgar/appclima/src/App.jsx
@@ -0,0 +1,9 @@
+console.log(import.meta.env.VITE_API_KEY);
+
+export default function App() {
+     return(
+      <>
+      <h1>My App</h1>
+      </>
+     )
+}
--- edgar/edgarvidio/src/main.jsx
+++ edgar/edgarvidio/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import react from 'react'
+import reactDOM from 'react-dom/client'
+import App from './App'
 
-import './estilos.css'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import './index.css';
+
+import { CssBaseline } from '@mui/material'
+import { SnackbarProvider } from 'notistack'
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SnackbarProvider>
+     <CssBaseline/>
+     <App/>
+    </SnackbarProvider>
+  </React.StrictMode>
+);
