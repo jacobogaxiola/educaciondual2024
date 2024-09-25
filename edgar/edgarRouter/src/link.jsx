37f3a3ff1404a38573dba1ac886e6a52cc750fe5
edgarvilla123 <edgar.soto@cecytesinaloa.edu.mx>
2024-09-25 16:36:05 -0700

    no puedo creer que de volar no tenga el poder...

---
+++ edgar/edgarRouter/src/link.jsx
@@ -0,0 +1,7 @@
+import { EVENTS } from "./consts.js";
+
+export function navigate(href) {
+  window.history.pushState({}, "", href);
+  const navigationEvent = new Event(EVENTS.PUSHSTATE);
+  window.dispatchEvent(navigationEvent);
+}
