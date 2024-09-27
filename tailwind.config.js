3c3e89accfcf4b37b20687fc7ee75bb0bed56a7d
ManuelUrias <manuel.urias@cecytesinaloa.edu.mx>
2024-09-26 17:13:30 -0700

    mmm

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/* @type {import('tailwindcss').Config} */  
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                anto: "'Anton', serif",
+                inter: "'Inter', serif",//
+            },
+
+        },
+    },
+    Plugins: [],
+}
