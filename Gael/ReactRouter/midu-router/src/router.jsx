68b1bb7b7c96861afbf0b8f0ec667de383abccb0
GaelGaer <gaelgalex02@gmail.com>
2024-09-26 17:08:31 -0700

    y el rocha

--- Gael/ReactRouter/midu-router/src/router.jsx
+++ Gael/ReactRouter/midu-router/src/router.jsx
@@ -1,5 +1,6 @@
 import { EVENTS } from './consts.js'
 import { useState, useEffect} from 'react'
+import { match } from 'path-to-regexp'
 
 export function Router ({ routes = [], defaultComponent: DefaultComponent = () => <h1>404</h1> }) {
     const [currentPath, setCurrentPath] = useState (window.location.pathname)
@@ -17,7 +18,21 @@ export function Router ({ routes = [], defaultComponent: DefaultComponent = () =
       window.removeEventListener(EVENTS.POPSTATE, onLocationChange)
     }
   }, [])
+
+  let routeParams  = {}
   
-  const Page = routes.find(({ path}) => path === currentPath)?.Component
-  return Page ? <Page /> : <DefaultComponent />
+  const Page = routes.find(({ path}) => {
+    if (path === currentPath) return true
+
+    const matcherUrl = match(path, { decode: decodeURIComponent })
+    const matched =matcherUrl(currentPath)
+    if (!matched) return false
+
+    routeParams = matched.params
+    return true
+  })?.Component
+  
+  return Page 
+  ? <Page routeParams={routeParams} /> 
+  : <DefaultComponent routeParams={routeParams} />
   }
