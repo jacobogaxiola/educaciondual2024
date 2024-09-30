7e72801d2d7c10c48c6d1bd4d97014562769cafd
ManuelUrias <manuel.urias@cecytesinaloa.edu.mx>
2024-09-30 15:08:30 -0700

    mmmm

--- Manuel/todo-app/tailwind.config.js
+++ Manuel/todo-app/tailwind.config.js
@@ -1,15 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
- content: [
+   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        anton: "'Anton', serif",
-        inter: "'Inter', serif",
-      }
-   }
+    extend: {},
   },
   plugins: [],
 }
--- Manuel/todo/src/components/Title/Title.jsx
+++
@@ -1,9 +0,0 @@
-const Title = () => {
-    return (
-        <h1 className="text-5xl font-anton font-bold tracking-widest">
-            Todo App
-        </h1>
-    );
-}
-
-export { Title }
