6066969f29cab6067821fc94ce8d270bc6d30176
edgarvilla123 <edgar.soto@cecytesinaloa.edu.mx>
2024-09-30 15:30:15 -0700

    hola

--- edgar/edgarRouter/src/link.jsx
+++
@@ -1,7 +0,0 @@
-import { EVENTS } from "./consts.js";
-
-export function navigate(href) {
-  window.history.pushState({}, "", href);
-  const navigationEvent = new Event(EVENTS.PUSHSTATE);
-  window.dispatchEvent(navigationEvent);
-}
