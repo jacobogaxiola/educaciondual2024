086ff9d2a08d9c41bf174952ecf8354ec04360bc
Edilene-Montoya <edilene.quintero@cecytesinaloa.edu.mx>
2024-10-03 14:16:58 -0700

    dccdc

--- ejemplo/react-router-app/src/components/Navbar.jsx
+++ ejemplo/react-router-app/src/components/Navbar.jsx
@@ -6,11 +6,8 @@ function Navbar() {
       <ul>
         <li><Link to="/Edilene">Edilene</Link></li>
         <li><Link to="/Janersy">Janersy</Link></li>
-<<<<<<< HEAD
         <li><Link to="/Gael">Gael</Link></li>
-=======
         <li><Link to="/Manuel">Manuel</Link></li>
->>>>>>> 878d896f574af0941179af27d06a67e88767963b
       </ul>
     </nav>
   );
