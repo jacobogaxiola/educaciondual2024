ea7ec662fb3a512cfc2edeab2a4a15fc5a2a8f26
Jonnaespinoza123 <jonnathan.espinoza@cecytesinaloa.edu.mx>
2024-10-03 16:42:41 -0700

    quiubo

---
+++ ejemplo/react-router-app/src/pages/Inicio.jsx
@@ -0,0 +1,11 @@
+import {  } from "react-router-dom";
+
+export default function inicio () {
+    return (
+      <>
+        <h1>Home</h1>
+        <p>blablablabla</p>
+        <Link to='/about' >Ir a ¿yelrocha?</Link>
+      </>
+    )
+  }
