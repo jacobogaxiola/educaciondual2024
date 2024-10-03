9d9f58aebfc8fe7c533fe6c26e130493353eba6e
kevinureta <yan.salazar@cecytesinaloa.edu.mx>
2024-10-03 14:15:48 -0700

    holaxs

--- ejemplo/react-router-app/src/App.jsx
+++ ejemplo/react-router-app/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Edilene  from './pages/Edilene';
 import Janersy  from './pages/Janersy';
+import Kevin  from './pages/Kevin';
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/Edilene" element={<Edilene />} />
           <Route path="/Janersy" element={<Janersy />} />
+          <Route path="/kevin" element={<Kevin />} />
+          
         </Routes>
       </div>
     </Router>
